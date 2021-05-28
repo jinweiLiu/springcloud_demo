4815dc28aebbdcf8789e07f27937441cd09caeb1
liujinwei <jwliu0213@163.com>
2021-05-28 14:40:55 +0800

    服务发现

--- payment8002/src/main/java/org/ecnu/ljw/controller/PaymentController.java
+++ payment8002/src/main/java/org/ecnu/ljw/controller/PaymentController.java
@@ -5,9 +5,12 @@ import org.ecnu.ljw.entity.CommonResult;
 import org.ecnu.ljw.entity.Payment;
 import org.ecnu.ljw.service.PaymentService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -22,6 +25,9 @@ public class PaymentController
 
     @Value("${server.port}")
     private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment)
@@ -62,5 +68,18 @@ public class PaymentController
         // 业务逻辑处理正确，但是需要耗费3秒钟
         try { TimeUnit.SECONDS.sleep(3); } catch (InterruptedException e) { e.printStackTrace(); }
         return serverPort;
+    }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for(String element : services){
+            log.info("-------element: "+element);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("payment-service");
+        for(ServiceInstance instance : instances){
+            log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+        }
+        return this.discoveryClient;
     }
 }
