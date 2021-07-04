bd23343ba5a76d7a8a684918a43193ed116e9fce
liujinwei <jwliu0213@163.com>
2021-07-04 13:09:58 +0800

    fallback and blockhandler

--- alibaba_consumer_order84/src/main/java/org/ecnu/ljw/controller/CircleBreakerController.java
+++ alibaba_consumer_order84/src/main/java/org/ecnu/ljw/controller/CircleBreakerController.java
@@ -1,6 +1,7 @@
 package org.ecnu.ljw.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.ecnu.ljw.entity.CommonResult;
 import org.ecnu.ljw.entity.Payment;
@@ -23,7 +24,10 @@ public class CircleBreakerController{
     //fallback管运行异常
     //blockhandler管配置违规
     @RequestMapping("/consumer/fallback/{id}")
-    @SentinelResource(value = "fallback")
+    //@SentinelResource(value = "fallback") //没有配置
+    //@SentinelResource(value = "fallback",fallback = "handlerFallback") //fallback只负责业务异常
+    //@SentinelResource(value = "fallback",blockHandler = "blockHandler") //blockHandler只负责sentinel控制台配置违规
+    @SentinelResource(value = "fallback", fallback = "handlerFallback", blockHandler = "blockHandler")
     public CommonResult<Payment> fallback(@PathVariable Long id){
         CommonResult<Payment> result = restTemplate.getForObject(SERVICE_URL + "/paymentSQL/"+id,CommonResult.class,id);
         if (id == 4) {            throw new IllegalArgumentException ("IllegalArgumentException,非法参数异常....");
@@ -31,5 +35,17 @@ public class CircleBreakerController{
             throw new NullPointerException ("NullPointerException,该ID没有对应记录,空指针异常");
         }
         return result;
+    }
+
+    //本例是fallback
+    public CommonResult handlerFallback(@PathVariable Long id, Throwable e){
+        Payment payment = new Payment(id,null);
+        return new CommonResult<>(444,"兜底异常handlerFallback,exception内容"+e.getMessage(),payment);
+    }
+
+    //本例是blockHandler
+    public CommonResult blockHandler(@PathVariable  Long id, BlockException blockException) {
+        Payment payment = new Payment(id,"null");
+        return new CommonResult<>(445,"blockHandler-sentinel限流,无此流水: blockException  "+blockException.getMessage(),payment);
     }
 }
